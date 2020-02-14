dbff908dc20cc2d1c0d20d7c364e6ff9d43a671b
Mike Sinnema <msinnema33@gmail.com>
2020-02-14 10:22:13 -0700

    working on display of data

--- client/src/App.js
+++ client/src/App.js
@@ -4,36 +4,61 @@ import Navigation from './components/Navigation';
 import Display from './components/Display';
 
 
-// import './components/TodoComponents/Todo.css';
-
-const playerList =[];
+const playerList =[
+  {
+    name: '',
+    country: '',
+    searches: '',
+    id: ''
+  }
+];
 
 class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
+    // constructor() {
+    //     super();
+        state = {
             playerlist: []
         };
-    }
+    // }
 
     componentDidMount(){
         axios
           .get('http://localhost:5000/api/players')
           .then(e => {
-            console.log('data: ', e);
+            console.log('list: ', e);
             this.setState({
               playerList: e.data
+              
             });
+            console.log(playerList);
+
           })
           .catch(err => console.log(err));
       }
-
+      
       render() {
           return (
               <div className='container'>
+                <header className='header'>
+                  <Navigation />
+                </header>
                   <h1>Mike's World Cup Sprint</h1>
-                <Navigation />
-                <Display playerList = {playerList} />
+
+                    <div className='players'>
+                      {this.state.playerlist.map(player => (
+                        <div className='playercard'>
+                          <p className='player' key={player.id}>
+                            {player.name}
+                          </p>
+                          <p className='country'>
+                            {player.country}
+                          </p>
+                          <p className='searchcount'>
+                            {player.searches}
+                          </p>
+                        </div>           
+                    ))} 
+                    </div>
               </div>
           )
       }
